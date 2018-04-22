96d95942e0dd350553a4fde4ae55ab688e5c1d42
TrocaTudo95 <pedromachado_95@sapo.pt>
2018-04-22 18:39:41 +0100

    fixed database

--- lbaw-laravel/app/Http/Controllers/UsersController.php
+++ lbaw-laravel/app/Http/Controllers/UsersController.php
@@ -9,4 +9,23 @@ class UsersController extends Controller
     public function profile($user_id){
         return user_id;
     }
+
+    public function destroy($user_id)
+   {
+       if($id != Auth::user()->id){
+            Notification::container()->error('You are not allowed to delete that user. WTF.');
+                        return Redirect::route('home');
+
+       }
+       if(User::find($user_id)){
+           $user = User::find($id);
+           $user->delete();
+           Notification::container()->success('Your account has been permanently removed from the system. Sorry to see you go!');
+           return Redirect::route('home');
+       } else {
+
+           return Redirect::route('home');
+       }
+   }
+
 }
