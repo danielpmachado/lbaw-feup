863bd60dff67a7944302affdf8baa67f15af5798
Sofia Alves <sofia@sofia-HP-Pavilion-Laptop-15-ck0xx.lan>
2018-05-09 23:57:03 +0100

    profile css added to laravel

--- lbaw-laravel/resources/views/user/profile.blade.php
+++ lbaw-laravel/resources/views/user/profile.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 
-<div class="container profile">
+<div class="container">
 	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-3">
 			<div class="profile-sidebar">
 				<div class="profile-user-pic">
 					<img src="images/default.png" alt="" class="img-fluid rounded">
@@ -16,33 +16,40 @@
 				</div>
 				<div class="profile-user-menu">
 					<ul class="nav flex-column nav-pills" role="tablist" aria-orientation="vertical">
-					<li class="active"><a data-toggle="pill" href="#home"><i class="fa fa-user"></i> Personal Info</a></li>
-					<li><a data-toggle="pill" href="#menu1"><i class="fa fa-heart"></i> Favorites</a></a></li>
-					<li><a data-toggle="pill" href="#menu2"><i class="fa fa-history"></i> History </a></li>
+					<li class="active"><a data-toggle="pill" href="#overview"><i class="fa fa-user"></i> Personal Info</a></li>
+					<li><a data-toggle="pill" href="#favorites"><i class="fa fa-heart"></i> Favorites</a></a></li>
+					<li><a data-toggle="pill" href="#history"><i class="fa fa-history"></i> History </a></li>
+					<li><a data-toggle="pill" href="#settings"><i class="fa fa-cogs"></i> Settings </a></li>
 					</ul>
 				</div>
 			</div>
 		</div>
-		<div class="tab-content col-md-8">
-			<div id="home" class="tab-pane fade show active">
-				<div class="panel panel-default target">
-					<div class="panel-heading" contenteditable="false">Personal Info</div>
-						<div class="panel-body">
-							<a> Username </a> <h5>{{$user->username}}</h5>
-							<a> Address </a> <h5>{{$user->address}}</h5>
-							<a> City </a> <h5>{{$user->city}}</h5>
-							<a> Email </a> <h5>{{$user->email}} </h5>
-							<a> Password</a> <h5> ********** </h5>
-							<a href="{{route('edit_profile', ['id' => Auth::user()->id])}}" type="button" class="btn btn-outline-primary"><span class="glyphicon glyphicon-pencil"></span>  Edit Info</a>
-							
-							<form action="{{ route('delete_user', ['id' => Auth::user()->id]) }}" method="post">
-								{{ csrf_field() }}
-								{{ method_field('DELETE') }}
-								<div class="form-group">
-									<button type="submit" class="btn btn-danger">DELETE</button>
-								</div>
-							</form>
-						</div>
+		<div class="profile-tabs tab-content col-md-9">
+			<div id="overview" class="tab-pane fade show active">
+				<div class="card fav-card">
+             		<div class="card-header bg-dark text-light">Personal Info</div>
+					<div class="card-body">
+						<a> Username </a> <p>{{$user->username}}</p>
+						<a> Address </a> <p>{{$user->address}}</p>
+						<a> City </a> <p>{{$user->city}}</p>
+						<a> Email </a> <p>{{$user->email}} </p>
+						<a> Password</a> <p> ********** </p>
+					</div>
+				</div>
+			</div>
+			<div id="settings" class="tab-pane fade show">
+				<div class="card fav-card">
+             		<div class="card-header bg-dark text-light">Personal Info</div>
+					<div class="card-body">
+						<a href="{{route('edit_profile', ['id' => Auth::user()->id])}}" type="button" class="btn btn-outline-primary"><span class="glyphicon glyphicon-pencil"></span>  Edit Info</a>
+						
+						<form action="{{ route('delete_user', ['id' => Auth::user()->id]) }}" method="post">
+							{{ csrf_field() }}
+							{{ method_field('DELETE') }}
+							<div class="form-group">
+								<button type="submit" class="btn btn-danger">DELETE</button>
+							</div>
+						</form>
 					</div>
 				</div>
 			</div>
