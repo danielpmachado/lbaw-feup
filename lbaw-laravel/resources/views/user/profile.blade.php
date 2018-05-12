3d5fe1d3c51e980d665fc11dc18220c8b6c08c7f
kyahra <sofiacbahamonde@gmail.com>
2018-05-12 17:32:54 +0100

    confirmation before deleting account

--- lbaw-laravel/resources/views/user/profile.blade.php
+++ lbaw-laravel/resources/views/user/profile.blade.php
@@ -77,14 +77,34 @@
 							</div>
 
 						</form>
+						<button type="button" class="btn btn-danger float-right" data-toggle="modal" data-target="#deleteAccount">
+						Delete Account
+						</button>
 
-						<form action="{{ route('delete_user', ['id' => Auth::user()->id]) }}" method="post">
-							{{ csrf_field() }}
-							{{ method_field('DELETE') }}
-							<div class="form-group float-right">
-								<button type="submit" class="btn btn-danger">Delete Account</button>
+						<!-- Modal -->
+						<div class="modal fade" id="deleteAccount" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+						<div class="modal-dialog modal-dialog-centered" role="document">
+							<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title" id="exampleModalLongTitle">Are you sure?</h5>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+								</button>
 							</div>
-						</form>
+							<div class="modal-body">
+								By deleting your account all your personal data will be lost and you will no longer be able to make purchases at Tech4U website.
+							</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+								<form action="{{ route('delete_user', ['id' => Auth::user()->id]) }}" method="post">
+									{{ csrf_field() }}
+									{{ method_field('DELETE') }}
+									<button type="submit" class="btn btn-danger">Delete</button>
+								</form>
+							</div>
+							</div>
+						</div>
+						</div>
 					</div>
 				</div>
 			</div>
