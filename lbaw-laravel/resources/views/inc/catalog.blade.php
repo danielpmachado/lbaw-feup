7bee32e0932f628a7f130b099a157721f0d2a86a
kyahra <sofiacbahamonde@gmail.com>
2018-05-26 12:16:07 +0100

    catalog started

--- lbaw-laravel/resources/views/inc/catalog.blade.php
+++ lbaw-laravel/resources/views/inc/catalog.blade.php
@@ -1,78 +1,34 @@
-
 <nav id="catalog-nav" class="navbar navbar-expand-md fixed-top navbar-dark bg-dark">
-        <a class="navbar-brand d-md-none">Catalog</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar2" aria-controls="navbar2" aria-expanded="false" aria-label="Toggle navigation">
-        <i class="fa fa-angle-down"></i>
-      </button>
+  <a class="navbar-brand d-md-none">Catalog</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar2" aria-controls="navbar2" aria-expanded="false" aria-label="Toggle navigation">
+      <i class="fa fa-angle-down"></i>
+    </button>
   
-      <div id="navbar2" class="collapse navbar-collapse " >
-        <ul class="navbar-nav  w-100 justify-content-between ">
-          <li class="nav-item">
+    <div id="navbar2" class="collapse navbar-collapse " >
+      <ul class="navbar-nav  w-100 justify-content-between ">
+        <li class="nav-item">
           <div class="dropdown">
-              <a class="nav-link" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
+              <a class="nav-link" href="#" id="Computers" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i class="fa fa-desktop"></i>Computers
               </a>
-              <div id="drop-1" class="dropdown-menu "  aria-labelledby="dropdownMenu2" >
+
+              <div id="drop-1" class="dropdown-menu"  aria-labelledby="dropdownMenu2" >
                 <div class="row mx-0 ">
                   <div class="col-sm-12 ">
-                    <h5>Notebooks</h5>
                     <ul class="list-group list-group-flush">
                       <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                        <a href="catalog.html">Asus</a>
+                        <a>Asus</a>
                         <span class="badge badge-dark badge-pill">14</span>
-                      </li>
-                      <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                        Apple
-                        <span class="badge badge-dark badge-pill">2</span>
-                      </li>
-                      <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                        Acer
-                        <span class="badge badge-dark badge-pill">1</span>
-                      </li>
-                      <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                        Acer
-                        <span class="badge badge-dark badge-pill">1</span>
-                      </li>
-                    </ul>
-                  </div>
-                  <div class="col-sm-6">
-                    <h5>Desktops</h5>
-                    <ul class="list-group list-group-flush">
-                      <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                        Asus
-                        <span class="badge badge-dark badge-pill">14</span>
-                      </li>
-                      <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                        Apple
-                        <span class="badge badge-dark badge-pill">2</span>
-                      </li>
-                      <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                        Acer
-                        <span class="badge badge-dark badge-pill">1</span>
-                      </li>
-                    </ul>
-                  </div>
-                  <div class="col-sm-6">
-                    <h5>Mobility</h5>
-                    <ul class="list-group list-group-flush">
-                      <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                        Asus
-                        <span class="badge badge-dark badge-pill">14</span>
-                      </li>
-                      <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                        Apple
-                        <span class="badge badge-dark badge-pill">2</span>
-                      </li>
-                      <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                        Acer
-                        <span class="badge badge-dark badge-pill">1</span>
                       </li>
                     </ul>
                   </div>
                 </div>
               </div>
             </div>
-              </li>
+          </li>
+
+
                 <li class="nav-item">
             <div class="dropdown">
                 <a class="nav-link" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -80,44 +36,7 @@
                 </a>
                 <div class="dropdown-menu"  aria-labelledby="dropdownMenu2">
                   <div class="row mx-0">
-                    <div class="col-sm-12">
-                      <h5>Notebooks</h5>
-                      <ul class="list-group list-group-flush">
-                        <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                          Asus
-                          <span class="badge badge-dark badge-pill">14</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                          Apple
-                          <span class="badge badge-dark badge-pill">2</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                          Acer
-                          <span class="badge badge-dark badge-pill">1</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                          Acer
-                          <span class="badge badge-dark badge-pill">1</span>
-                        </li>
-                      </ul>
-                    </div>
-                    <div class="col-sm-12">
-                      <h5>Desktops</h5>
-                      <ul class="list-group list-group-flush">
-                        <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                          Asus
-                          <span class="badge badge-dark badge-pill">14</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                          Apple
-                          <span class="badge badge-dark badge-pill">2</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                          Acer
-                          <span class="badge badge-dark badge-pill">1</span>
-                        </li>
-                      </ul>
-                    </div>
+
                   </div>
                 </div>
               </div>
@@ -129,44 +48,7 @@
                 </a>
                 <div class="dropdown-menu"  aria-labelledby="dropdownMenu2">
                   <div class="row mx-0">
-                    <div class="col-sm-6">
-                      <h5>Notebooks</h5>
-                      <ul class="list-group list-group-flush">
-                        <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                          Asus
-                          <span class="badge badge-dark badge-pill">14</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                          Apple
-                          <span class="badge badge-dark badge-pill">2</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                          Acer
-                          <span class="badge badge-dark badge-pill">1</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                          Acer
-                          <span class="badge badge-dark badge-pill">1</span>
-                        </li>
-                      </ul>
-                    </div>
-                    <div class="col-sm-6">
-                      <h5>Desktops</h5>
-                      <ul class="list-group list-group-flush">
-                        <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                          Asus
-                          <span class="badge badge-dark badge-pill">14</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                          Apple
-                          <span class="badge badge-dark badge-pill">2</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                          Acer
-                          <span class="badge badge-dark badge-pill">1</span>
-                        </li>
-                      </ul>
-                    </div>
+
                   </div>
                 </div>
               </div>
@@ -178,27 +60,7 @@
                 </a>
                 <div class="dropdown-menu"  aria-labelledby="dropdownMenu2">
                   <div class="row mx-0">
-                    <div class="col-sm-12">
-                      <h5>Notebooks</h5>
-                      <ul class="list-group list-group-flush">
-                        <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                          Asus
-                          <span class="badge badge-dark badge-pill">14</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                          Apple
-                          <span class="badge badge-dark badge-pill">2</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                          Acer
-                          <span class="badge badge-dark badge-pill">1</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                          Acer
-                          <span class="badge badge-dark badge-pill">1</span>
-                        </li>
-                      </ul>
-                    </div>
+
                   </div>
                 </div>
               </div>
@@ -210,27 +72,7 @@
                 </a>
                 <div id="drop-2" class="dropdown-menu"  aria-labelledby="dropdownMenu2">
                   <div class="row mx-0">
-                    <div class="col-sm-12">
-                      <h5>Notebooks</h5>
-                      <ul class="list-group list-group-flush">
-                        <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                          <a href="catalog.html">Asus</a>
-                          <span class="badge badge-dark badge-pill">14</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                          Apple
-                          <span class="badge badge-dark badge-pill">2</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                          Acer
-                          <span class="badge badge-dark badge-pill">1</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between align-items-center p-1">
-                          Acer
-                          <span class="badge badge-dark badge-pill">1</span>
-                        </li>
-                      </ul>
-                    </div>
+                    
                   </div>
                 </div>
               </div>
@@ -238,4 +80,3 @@
         </ul>
      </div>
   </nav>
-  
