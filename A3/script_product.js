37ce082973db4cc3735ed4907a4622c4c111846a
David Falcão <davidrsfalcao@gmail.com>
2018-05-28 15:50:31 +0100

    fix product_admin page

--- A3/script_product.js
+++ A3/script_product.js
@@ -1,27 +1,45 @@
-
+var backup = [document.getElementsByClassName('text-editable').length];
 
 function setEditable() {
     var elems = document.getElementsByClassName('text-editable');
 
-    for(var i=0; i<elems.length; i++){
+    for(let i=0; i<elems.length; i++){
         elems[i].contentEditable = "true";
+        backup[i] = elems[i].innerHTML;
     }
 
     document.getElementById("saveChangesProdut").style.display = "inline";
+    document.getElementById("cancelChangesProdut").style.display = "inline";
     document.getElementById("editProdut").style.display = "none";
 
 }
 
-function setNotEditable(){
+function saveChangesProdut(){
+    var elems = document.getElementsByClassName('text-editable');
+
+    for(let i=0; i<elems.length; i++){
+        elems[i].contentEditable = "false";
+    }
+
+    //Guardar database
+    document.getElementById("cancelChangesProdut").style.display = "none";
+    document.getElementById("saveChangesProdut").style.display = "none";
+    document.getElementById("editProdut").style.display = "inline";
+}
+
+function cancelChangesProdut(){
     var elems = document.getElementsByClassName('text-editable');
 
     for(var i=0; i<elems.length; i++){
         elems[i].contentEditable = "false";
+        elems[i].innerHTML = backup[i];
     }
 
+    document.getElementById("cancelChangesProdut").style.display = "none";
     document.getElementById("saveChangesProdut").style.display = "none";
     document.getElementById("editProdut").style.display = "inline";
 }
+
 
 jQuery(document).ready(function($) {
   var alterClass = function() {
