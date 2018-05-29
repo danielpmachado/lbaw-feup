763884b7cb188570c7c2d609c449dde7d26b441d
TrocaTudo95 <pedromachado_95@sapo.pt>
2018-05-29 15:34:48 +0100

    bug on google login

--- lbaw-laravel/routes/web.php
+++ lbaw-laravel/routes/web.php
@@ -17,6 +17,7 @@ Route::get('/about', 'PagesController@about');
 Route::get('/faq', 'PagesController@faq');
 Route::get('/404', 'PagesController@error404');
 
+Route::get('/search_page','PagesController@search_page');
 // User
 Route::get('/users/{id}','UsersController@profile')->name('profile');
 Route::post('/users/{id}/update','UsersController@update')->name('update_user');
@@ -46,6 +47,7 @@ Route::get('/products/{id}','ProductController@page')->name('page');
 Route::post('/products/{id}/favorite','ProductController@favorite');
 Route::post('/products/{id}/unfavorite','ProductController@unfavorite');
 Route::put('/products/{id}/reviews', 'ReviewController@create');
+
 
 
 //Admin
