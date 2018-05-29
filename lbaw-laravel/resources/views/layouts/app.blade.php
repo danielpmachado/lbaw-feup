f9ce0c70c9f62076655e984841b67bbee401e744
David Falcão <davidrsfalcao@gmail.com>
2018-05-29 23:50:35 +0100

    add favicon

--- lbaw-laravel/resources/views/layouts/app.blade.php
+++ lbaw-laravel/resources/views/layouts/app.blade.php
@@ -19,6 +19,28 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat:700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="{{ URL::to('/') }}/images/favicon/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ URL::to('/') }}/images/favicon/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ URL::to('/') }}/images/favicon/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ URL::to('/') }}/images/favicon/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="{{ URL::to('/') }}/images/favicon/apple-touch-icon-60x60.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="{{ URL::to('/') }}/images/favicon/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="{{ URL::to('/') }}/images/favicon/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="{{ URL::to('/') }}/images/favicon/apple-touch-icon-152x152.png" />
+    <link rel="icon" type="image/png" href="{{ URL::to('/') }}/images/favicon/favicon-196x196.png" sizes="196x196" />
+    <link rel="icon" type="image/png" href="{{ URL::to('/') }}/images/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/png" href="{{ URL::to('/') }}/images/favicon/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="{{ URL::to('/') }}/images/favicon/favicon-16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="{{ URL::to('/') }}/images/favicon/favicon-128.png" sizes="128x128" />
+    <meta name="application-name" content="&nbsp;"/>
+    <meta name="msapplication-TileColor" content="#FFFFFF" />
+    <meta name="msapplication-TileImage" content="{{ URL::to('/') }}/images/favicon/mstile-144x144.png" />
+    <meta name="msapplication-square70x70logo" content="{{ URL::to('/') }}/images/favicon/mstile-70x70.png" />
+    <meta name="msapplication-square150x150logo" content="{{ URL::to('/') }}/images/favicon/mstile-150x150.png" />
+    <meta name="msapplication-wide310x150logo" content="{{ URL::to('/') }}/images/favicon/mstile-310x150.png" />
+    <meta name="msapplication-square310x310logo" content="{{ URL::to('/') }}/images/favicon/mstile-310x310.png" />
+
+
     <!-- Scripts -->
     <script type="text/javascript" src={{ asset('js/app.js') }} defer></script>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
