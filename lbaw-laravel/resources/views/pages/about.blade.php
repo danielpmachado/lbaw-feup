1f88b84a4885115d89043fed2aaa494ceb7427ba
David Falcão <davidrsfalcao@gmail.com>
2018-05-29 23:33:31 +0100

    update about css

--- lbaw-laravel/resources/views/pages/about.blade.php
+++ lbaw-laravel/resources/views/pages/about.blade.php
@@ -1,170 +1,166 @@
 @extends('layouts.app')
 
 @section('content')
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 <div id="about" class="container text-center">
-    <div class="row">
-        <h1 id="about-title" class="container text-center">Tech4U</h1>
-        <p>
-          Donec sollicitudin quis felis vel iaculis. Etiam nulla tellus, volutpat nec varius congue, vulputate ut magna. Suspendisse et tellus posuere, tincidunt massa ac, malesuada velit. Pellentesque metus eros, fermentum sit amet augue ac, faucibus aliquet neque. Nam faucibus ullamcorper mi, ac pharetra eros pharetra nec. Cras sed eros vitae diam mattis lacinia quis non risus. Donec a rutrum turpis. Pellentesque sit amet odio nec odio rhoncus ullamcorper at nec sem.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
-  </div>
-  
-  <div id="info" class="container text-center">
-       <div class="row">
-         <div class="col-sm-4">
-           <i class="fa fa-thumbs-up"></i>
-           <h4>QUALITY</h4>
-           <p>In condimentum lacinia magna ut euismod. Sed nec lorem nec metus tincidunt auctor nec id sem.</p>
-         </div>
-         <div class="col-sm-4">
-           <i class="fa fa-lock"></i>
-           <h4>SECURITY</h4>
-           <p>In condimentum lacinia magna ut euismod. Sed nec lorem nec metus tincidunt auctor nec id sem.</p>
-         </div>
-         <div class="col-sm-4">
-           <i class="fa fa-dollar"></i>
-           <h4>LOW PRICES</h4>
-           <p>In condimentum lacinia magna ut euismod. Sed nec lorem nec metus tincidunt auctor nec id sem.</p>
-         </div>
-       </div>
-       <div class="row">
-         <div class="col-sm-4">
-           <i class="fa fa-heart"></i>
-           <h4>DEDICATION</h4>
-           <p>In condimentum lacinia magna ut euismod. Sed nec lorem nec metus tincidunt auctor nec id sem.</p>
-         </div>
-         <div class="col-sm-4">
-           <i class="fa fa-medkit"></i>
-           <h4>SUPPORT</h4>
-           <p>In condimentum lacinia magna ut euismod. Sed nec lorem nec metus tincidunt auctor nec id sem.</p>
-         </div>
-         <div class="col-sm-4">
-           <span class="fa fa-wrench"></span>
-           <h4 style="color:#303030;">HARD WORK</h4>
-           <p>In condimentum lacinia magna ut euismod. Sed nec lorem nec metus tincidunt auctor nec id sem.</p>
-         </div>
-       </div>
-     </div>
-  
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-  <section class="team">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-10 offset-md-1">
-          <div class="col-lg-12">
-            <h2 class="description text-center" id="team">Team</h2>
-            <div class="row pt-md">
-              <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
-                <div class="img-box rounded">
-                  <img src="images/daniel2.jpg" class="img-fluid rounded">
-                  <ul class="text-center">
-                    <a href="#"><li><i class="fa fa-facebook"></i></li></a>
-                    <a href="#"><li><i class="fa fa-instagram"></i></li></a>
-                    <a href="#"><li><i class="fa fa-github"></i></li></a>
-                  </ul>
-                </div>
-                <h1>Daniel Machado</h1>
-                <h2>Co-founder/ Operations</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-              </div>
-              <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
-                <div class="img-box">
-                  <img src="images/david2.jpg" class="img-fluid rounded">
-                  <ul class="text-center">
-                    <a href="#"><li><i class="fa fa-facebook"></i></li></a>
-                    <a href="#"><li><i class="fa fa-instagram"></i></li></a>
-                    <a href="#"><li><i class="fa fa-github"></i></li></a>
-                  </ul>
-                </div>
-                <h1>David Falcão</h1>
-                <h2>Co-founder/ Projects</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-              </div>
-              <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
-                <div class="img-box">
-                  <img src="images/troca2.jpg" class="img-fluid rounded">
-                  <ul class="text-center">
-                    <a href="#"><li><i class="fa fa-facebook"></i></li></a>
-                    <a href="#"><li><i class="fa fa-instagram"></i></li></a>
-                    <a href="#"><li><i class="fa fa-github"></i></li></a>
-                  </ul>
-                </div>
-                <h1>José Pedro Machado</h1>
-                <h2>Co-founder/ Marketing</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-              </div>
-              <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
-                <div class="img-box">
-                  <img src="images/sofia2.jpg" class="img-fluid rounded">
-                  <ul class="text-center">
-                    <a href="#"><li><i class="fa fa-facebook"></i></li></a>
-                    <a href="#"><li><i class="fa fa-instagram"></i></li></a>
-                    <a href="#"><li><i class="fa fa-github"></i></li></a>
-                  </ul>
-                </div>
-                <h1>Sofia Alves</h1>
-                <h2>Designer</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-              </div>
-            </div>
-          </div>
+    <div class="row text-center description-row">
+        <div class="col-md-2">
         </div>
-      </div>
+        <div class="col-md-8">
+            <h1 id="about-title" class="container text-center">Tech4U</h1>
+            <p>Donec sollicitudin quis felis vel iaculis. Etiam nulla tellus, volutpat nec varius congue, vulputate ut magna. Suspendisse et tellus posuere, tincidunt massa ac, malesuada velit. Pellentesque metus eros, fermentum sit amet augue ac, faucibus aliquet neque. Nam faucibus ullamcorper mi, ac pharetra eros pharetra nec. Cras sed eros vitae diam mattis lacinia quis non risus. Donec a rutrum turpis. Pellentesque sit amet odio nec odio rhoncus ullamcorper at nec sem.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="col-md-2">
+        </div>
     </div>
-  </section>
-  
-  <section id="contact">
-     <div class="container fluid">
-  
-  
-         <h3 class="text text-center">Contact Us</h3>
-         <div class="row">
-           <div class="col-sm-12 col-md-6 col-lg-4">
-             <div class="card border-0">
-                <div class="card-body text-center">
-                  <i class="fa fa-phone fa-5x mb-3" aria-hidden="true"></i>
-                  <h5 class="text-uppercase mb-5">+1 555 123456</h5>
-  
+
+
+    <div id="info" class="container text-center">
+        <div class="row info-row">
+            <div class="col-md-4 info-point">
+                <i class="fa fa-thumbs-up"></i>
+                <p><span class="category">Quality</span> is the cornerstone of our activities and internal processes</p>
+            </div>
+            <div class="col-md-4 info-point">
+                <i class="fa fa-lock"></i>
+                <p>We take your <span class="category">security</span> to heart. All your information is confidential</p>
+            </div>
+            <div class="col-md-4 info-point">
+                <i class="fa fa-dollar"></i>
+                <p>We analyze the market to give you the <span class="category">lowest prices</span> for all products</p>
+            </div>
+        </div>
+
+        <div class="row info-row">
+            <div class="col-md-4 info-point dedication">
+                <i class="fa fa-heart"></i>
+                <p>You can count on our <span class="category">dedication</span> to give you the best possible experience</p>
+            </div>
+            <div class="col-md-4 info-point hard-work">
+                <i class="fa fa-wrench"></i>
+                <p>We <span class="category">work hard</span> to always keep all the information up to date</p>
+            </div>
+            <div class="col-md-4 info-point support">
+                <i class="fa fa-medkit"></i>
+                <p>If you have any questions, do not hesitate to contact us, we will give you <span class="category">work hard</span> for life.</p>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<section class="team">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-10 offset-md-1">
+                <div class="col-lg-12">
+                    <h2 class="description text-center" id="team">Our team</h2>
+                    <div class="row pt-md">
+                        <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
+                            <div class="img-box rounded">
+                                <img src="images/daniel2.jpg" class="img-fluid rounded">
+                                <ul class="text-center">
+                                    <a href="https://www.facebook.com/daniel.pmachado" target="_blank"><li><i class="fa fa-facebook"></i></li></a>
+                                    <a href="https://www.instagram.com/daniel.pmachado/" target="_blank"><li><i class="fa fa-instagram"></i></li></a>
+                                    <a href="https://github.com/dolfander" target="_blank"><li><i class="fa fa-github"></i></li></a>
+                                </ul>
+                            </div>
+                            <h1>Daniel Machado</h1>
+                            <h2>Co-founder/ Operations</h2>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
+                            <div class="img-box">
+                                <img src="images/david2.jpg" class="img-fluid rounded">
+                                <ul class="text-center">
+                                    <a href="https://www.facebook.com/david.falcao2" target="_blank"><li><i class="fa fa-facebook"></i></li></a>
+                                    <a href="https://www.instagram.com/davidrsfalcao/" target="_blank"><li><i class="fa fa-instagram"></i></li></a>
+                                    <a href="https://github.com/davidrsfalcao" target="_blank"><li><i class="fa fa-github"></i></li></a>
+                                </ul>
+                            </div>
+                            <h1>David Falcão</h1>
+                            <h2>Co-founder/ Projects</h2>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
+                            <div class="img-box">
+                                <img src="images/troca2.jpg" class="img-fluid rounded">
+                                <ul class="text-center">
+                                    <a href="https://www.facebook.com/zepedro.machado.9" target="_blank"><li><i class="fa fa-facebook"></i></li></a>
+                                    <a href="https://www.instagram.com/zepedromachado97/" target="_blank"><li><i class="fa fa-instagram"></i></li></a>
+                                    <a href="https://github.com/TrocaTudo95" target="_blank"><li><i class="fa fa-github"></i></li></a>
+                                </ul>
+                            </div>
+                            <h1>José Pedro Machado</h1>
+                            <h2>Co-founder/ Marketing</h2>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
+                            <div class="img-box">
+                                <img src="images/sofia2.jpg" class="img-fluid rounded">
+                                <ul class="text-center">
+                                    <a href="https://www.facebook.com/kyahraa" target="_blank"><li><i class="fa fa-facebook"></i></li></a>
+                                    <a href="https://www.instagram.com/sofiacbalves/" target="_blank"><li><i class="fa fa-instagram"></i></li></a>
+                                    <a href="https://github.com/Kyahra" target="_blank"><li><i class="fa fa-github"></i></li></a>
+                                </ul>
+                            </div>
+                            <h1>Sofia Alves</h1>
+                            <h2>Designer</h2>
+                        </div>
+                    </div>
                 </div>
-              </div>
-           </div>
-           <div class="col-sm-12 col-md-6 col-lg-4">
-             <div class="card border-0">
-                <div class="card-body text-center">
-                  <i class="fa fa-map-marker fa-5x mb-3" aria-hidden="true"></i>
-                  <h5 class="text-uppercase mb-5"> Rua Dr Roberto Frias, FEUP </h5>
-  
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="contact">
+    <div class="container fluid">
+        <h2 class="text text-center">Contact Us</h2>
+        <div class="row">
+            <div class="col-sm-12 col-md-6 col-lg-4">
+                <div class="card border-0">
+                    <div class="card-body text-center">
+                        <i class="fa fa-phone" aria-hidden="true"></i>
+                        <h5>+1 555 123456</h5>
+
+                    </div>
                 </div>
-              </div>
-           </div>
-           <div class="col-sm-12 col-md-6 col-lg-4">
-             <div class="card border-0">
-                <div class="card-body text-center">
-                  <i class="fa fa-envelope fa-5x mb-3" aria-hidden="true"></i>
-                  <h5 class="text-uppercase mb-5">tech4you@gmail.com</h5>
-  
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-4">
+                <div class="card border-0">
+                    <div class="card-body text-center">
+                        <i class="fa fa-map-marker" aria-hidden="true"></i>
+                        <h5> Rua Dr Roberto Frias, FEUP </h5>
+
+                    </div>
                 </div>
-              </div>
-           </div>
-         </div>
-  
-     </div>
-  
-  
-     <div id="map" style="width:100%;height:400px;"></div>
-  
-  <script>
-  function myMap() {
-    var mapCanvas = document.getElementById("map");
-    var mapOptions = {
-      center: new google.maps.LatLng(41.1780, -8.5980), zoom: 15
-    };
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-  }
-  </script>
-  
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDCkTadtGML5L8mAOKPp79NUcQ9etEh6KE&callback=myMap"></script>
-  
-@endsection  
-      
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-4">
+                <div class="card border-0">
+                    <div class="card-body text-center">
+                        <i class="fa fa-envelope"></i>
+                        <h5>tech4you@gmail.com</h5>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>    
+
+
+    <div id="map" style="width:100%;height:400px;"></div>
+
+    <script>
+    function myMap() {
+        var mapCanvas = document.getElementById("map");
+        var mapOptions = {
+            center: new google.maps.LatLng(41.1780, -8.5980), zoom: 15
+        };
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+    }
+    </script>
+
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDCkTadtGML5L8mAOKPp79NUcQ9etEh6KE&callback=myMap"></script>
+
+    @endsection
