5ca7d146b5c05c8512376ea25717c8bad4ad0fa6
kyahra <sofiacbahamonde@gmail.com>
2018-05-30 01:21:03 +0100

    cart expedition

---
+++ lbaw-laravel/resources/views/cart/expedition.blade.php
@@ -0,0 +1,38 @@
+<div role="tabpanel" class="tab-pane" id="step-2">
+    <div class="container-fluid" style="width:70%;">
+        <div class="card shopping-cart">
+            <div class="card-header bg-dark text-light">Expedition Info</div>
+            <div class="card-body">
+                <form class="form-horizontal">
+                
+                    <div class="form-group">
+                        <a> Address </a>
+                        <input  id="address" type="text" class="form-control" name="address" value="{{ $user->address }}" required>
+                    </div>
+                
+                    <div class="form-row">
+                        <div class="form-group col-md-8 ">
+                            <a> City </a>
+                            <input  id="city" type="text" class="form-control" name="city" value="{{ $user->city }}" required>
+                        </div>
+                        <div class="form-group col-md-4">
+                            <a> Zip </a>
+                            <input  id="zip" type="text" class="form-control" name="zip" value="{{ $user->zip }}" required>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <a> Email </a>
+                        <input  id="email" type="text" class="form-control" name="email" value="{{ $user->email }}" required>
+                    </div>
+                </form>
+                </div>
+                <div class="card-footer ">
+                    <button id="step-2-next"  type="submit" class="btn btn-outline-dark float-right">
+                        Next
+                    </button>
+                </div>
+            
+        </div>
+    </div>
+</div>
---
+++ lbaw-laravel/resources/views/cart/payment.blade.php
@@ -0,0 +1,40 @@
+<div role="tabpanel" class="tab-pane" id="step-3">
+    <div class="container-fluid" style="width:60%;">
+        <div class="card shopping-cart">
+            <div class="card-header bg-dark text-light">Expedition Info</div>
+            <div class="card-body">
+                <form class="form-horizontal">
+                    <fieldset class="form-group">
+                        <div class="row">
+                            <div class="col-sm-10">
+                                <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="visa">
+                                <label class="form-check-label" for="gridRadios1">
+                                    First radio
+                                </label>
+                                </div>
+                                <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="mastercard">
+                                <label class="form-check-label" for="gridRadios2">
+                                    Second radio
+                                </label>
+                                </div>
+                                <div class="form-check disabled">
+                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="americanexpress">
+                                <label class="form-check-label" for="gridRadios3">
+                                    Third disabled radio
+                                </label>
+                                </div>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+            <div class="card-footer ">
+                <button id="step-3-next"  type="submit" class="btn btn-outline-dark float-right">
+                    Next
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
