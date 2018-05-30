6eb2aebc730860bbf24878fec0f681c229fdf0f6
TrocaTudo95 <pedromachado_95@sapo.pt>
2018-05-30 10:22:15 +0100

    fixed bug

--- lbaw-laravel/app/Http/Controllers/ProductController.php
+++ lbaw-laravel/app/Http/Controllers/ProductController.php
@@ -45,13 +45,8 @@ class ProductController extends Controller
     }
 
     public function searchByName($searchText){
-       // return DB::select("SELECT * from product
-       //  WHERE textsearchable_name_col @@ to_tsquery('portuguese',?) LIMIT 20",[$searchText]);
-
-        $products = Product::whereRaw('textsearchable_name_col @@ to_tsquery(\'portuguese\', ?)', $searchText);
-        //
-        // dd($products);
-        return $products;
+       return DB::select("SELECT * from product
+        WHERE textsearchable_name_col @@ to_tsquery('portuguese',?) LIMIT 20",[$searchText]);
     }
 
 
